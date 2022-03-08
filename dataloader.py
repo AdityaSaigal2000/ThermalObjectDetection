39f4210b65d2a4532c625b4c642ca64c272b7c59
AdityaSaigal2000 <46829655+AdityaSaigal2000@users.noreply.github.com>
2022-03-07 22:36:03 -0500

    Add functionality to choose between thermal and rgb

--- dataloader.py
+++ dataloader.py
@@ -3,11 +3,14 @@ import os
 import torch
 import matplotlib.image as mpimg
 import torchvision
-
+from torchvision import transforms
+import numpy as np
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 class Image_Dataset(torch.utils.data.Dataset):
-    def __init__(self, root, filename, target_categories):
+    def __init__(self, root, filename, target_categories, thermal = True):
+        # if thermal is true, then setup a dataloader with 3 channel thermal images (replicated)
+        self.thermal = thermal
         boxes, classes, files = {}, {}, {}
         self.root = root
         self.boxes, self.classes, self.files = [], [], []
@@ -22,24 +25,24 @@ class Image_Dataset(torch.utils.data.Dataset):
                         del classes[curr_id]
                         del files[curr_id]
                 curr_id = label["image_id"]
-                
+
                 boxes[curr_id] = []
                 classes[curr_id] = []
 
                 for image in data["images"]:
                     if(image["id"] == curr_id):
                         files[curr_id] = image["file_name"]
-                
-            if(label["category_id"] in target_categories):        
-                classes[curr_id].append(target_categories[label["category_id"]])
-                
-                boxes[curr_id].append([label["bbox"][0], label["bbox"][1], label["bbox"][0] + label["bbox"][2], label["bbox"][1] + label["bbox"][3]])
-            
 
-        
+            if(label["category_id"] in target_categories):
+                classes[curr_id].append(target_categories[label["category_id"]])
+
+                boxes[curr_id].append([label["bbox"][0], label["bbox"][1], label["bbox"][0] + label["bbox"][2], label["bbox"][1] + label["bbox"][3]])
+
+
+
         for i, idx in enumerate(files):
-            if(i == 600):
-                break
+            #if(i == 8000):
+            #    break
             self.files.append(files[idx])
             self.classes.append(classes[idx])
             self.boxes.append(boxes[idx])
@@ -49,9 +52,22 @@ class Image_Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         filename, boxes, classes = self.files[idx], torch.as_tensor(self.boxes[idx]), torch.as_tensor(self.classes[idx])
+
         img_path = os.path.join(self.root, filename)
-        
-        img = torch.as_tensor(torchvision.transforms.functional.to_tensor(mpimg.imread(img_path).copy()))
+        img = mpimg.imread(img_path)
+        if(self.thermal):
+            img = np.reshape(img, (1, img.shape[0], img.shape[1]))
+            img_3_channel = np.concatenate((img, img, img), axis = 0)
+            img = torch.as_tensor(img_3_channel).div(255)
+        else:
+            transform = transforms.Compose([transforms.ToTensor()])
+            img = transform(img)
+
+        #print(img.shape)
+        #exit()
+        #img = torch.as_tensor(torchvision.transforms.functional.to_tensor(img_3_channel.copy()))
+        #print(img.shape)
+        #exit()
 
         target = {}
         target["boxes"] = boxes
@@ -76,4 +92,3 @@ if __name__ == "__main__":
         ax.add_patch(rect)
     print(target)
     plt.savefig("./test.png")
-
